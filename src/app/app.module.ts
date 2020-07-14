bb9c26b8e051be537cb6fac7d8e165f70a866ffd
Mateusz Kopij <sagasu@gmail.com>
2020-07-14 12:23:02 +0100

    adding relative time pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,12 +10,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LaunchListComponent } from './components/launch-list/launch-list.component';
 import { LaunchDetailsComponent } from './components/launch-details/launch-details.component';
+import { RelativeTimePipe } from './pipes/relative-time.pipe';
 import { CommonModule } from '@angular/common';
 
 
 @NgModule({
   declarations: [
-    AppComponent, LaunchListComponent, LaunchDetailsComponent
+    AppComponent, LaunchListComponent, LaunchDetailsComponent, RelativeTimePipe
   ],
   imports: [
     CommonModule,
